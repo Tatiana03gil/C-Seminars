8e473b79c93d4b280cdd77b9f4de47aac50e799c
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2023-07-04 21:16:48 +0300

    Задача на квадраты

--- Sem_1/Program.cs
+++ Sem_1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//int a = 25;
+//double b = 2.5;
+//string c = "Hello";
+//bool d = false;
+
+//int number = 5;
+
+//Console.WriteLine("My namber is " + number + " (five)"); //конкотинация
+//Console.WriteLine($"My namber is {number} (five)"); //форматирование
+
+//Console.Write ("Введите данные: ");
+//int num = Convert.ToInt32(Console.ReadLine ()); 
+// конвертирует вводимое число (строку) в число
+//Console.WriteLine ("Наше число это " + num);
+
+//Напишите программу, которая на вход принимает два
+//числа и проверяет, является ли первое число квадратом
+//второго.
+
+int num1, num2;
+Console.WriteLine ("Введите первое число: ");
+num1 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите первое число: ");
+num2 = Convert.ToInt32 (Console.ReadLine ());
+int kvad = num2*num2;
+
+if (num1 == kvad)
+ {
+    Console.WriteLine ("true");
+ }
+    else 
+ {
+    Console.WriteLine ("false");
+ }
