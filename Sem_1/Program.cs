4d115dd94b91a78404a02c7ff02ad623bcc3c693
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2023-07-04 21:37:36 +0300

    Задача на вывод целых чисел от -Н до Н

--- Sem_1/Program.cs
+++ Sem_1/Program.cs
@@ -17,18 +17,24 @@
 //числа и проверяет, является ли первое число квадратом
 //второго.
 
-int num1, num2;
+/*int num1, num2;
 Console.WriteLine ("Введите первое число: ");
 num1 = Convert.ToInt32 (Console.ReadLine ());
 Console.WriteLine ("Введите первое число: ");
 num2 = Convert.ToInt32 (Console.ReadLine ());
-int kvad = num2*num2;
 
-if (num1 == kvad)
- {
-    Console.WriteLine ("true");
- }
-    else 
- {
-    Console.WriteLine ("false");
- }
+if (num1 == num2*num2) Console.WriteLine ("true");
+     else Console.WriteLine ("false");
+*/
+
+/*Напишите программу, которая на вход принимает одно число
+ (N), а на выходе показывает все целые числа в промежутке
+  от -N до N.*/
+
+Console.WriteLine ("Введите число: ");
+int N = Convert.ToInt32 (Console.ReadLine ());
+int Count = -N;
+while (Count <= N) {
+    Console.Write (Count + ", ");
+    Count = Count+1;
+}
