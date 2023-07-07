45d1307a8a1557a3b3e9e41b7fe47df9e1414941
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2023-07-07 20:48:39 +0300

    Задача на удаление десятков

---
+++ Sem_2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая выводит случайное
+трёхзначное число и удаляет вторую цифру этого числа.
+*/
+
+int CutNumber (int num)
+{
+    int hundreds = num / 100;
+    int units = num % 10;
+    int result = hundreds*10 + units;
+    return result;
+}
+
+int number = new Random().Next(100,1000);
+Console.WriteLine ("Наше случайное число: " + number);
+int newNumber = CutNumber (number);
+Console.WriteLine ("Новая версия числа: " + newNumber);
