cbb2b7f9392f0f166d4a1a3932b77622044228f6
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2023-07-07 21:09:32 +0300

    Задача на два делителя

--- Sem_2/Program.cs
+++ Sem_2/Program.cs
@@ -2,7 +2,7 @@
 трёхзначное число и удаляет вторую цифру этого числа.
 */
 
-int CutNumber (int num)
+/*int CutNumber (int num)
 {
     int hundreds = num / 100;
     int units = num % 10;
@@ -14,3 +14,21 @@ int number = new Random().Next(100,1000);
 Console.WriteLine ("Наше случайное число: " + number);
 int newNumber = CutNumber (number);
 Console.WriteLine ("Новая версия числа: " + newNumber);
+*/
+
+/*Напишите программу, которая принимает на вход число и проверяет,
+кратно ли оно одновременно a и b.*/
+
+bool IsDividable (int number, int a, int b){
+if (number % a == 0 && number % b == 0) return true;
+else return false;
+} 
+
+Console.WriteLine ("Введите число: ");
+int num = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите первый делитель: ");
+int div1 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите второй делитель: ");
+int div2 = Convert.ToInt32 (Console.ReadLine ());
+bool result = IsDividable (num, div1, div2);
+Console.WriteLine (result);
