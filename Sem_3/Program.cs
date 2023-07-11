9756dc0ab50f57d8fe6f395f5ad2d3f1aea97d9c
Tatiana Ihnatchyk <tatiana03gil@gmail.com>
2023-07-11 20:31:17 +0300

    Задача 1

--- Sem_3/Program.cs
+++ Sem_3/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿/*Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).*/
+
+void ShowDiapazone (int quad)
+{
+    if (quad == 1) Console.WriteLine ("Диапазон х > 0 и y > 0");
+    else if (quad == 2) Console.WriteLine ("Диапазон х < 0 и y > 0");
+    else if (quad == 3) Console.WriteLine ("Диапазон х < 0 и y < 0");
+    else if (quad == 4) Console.WriteLine ("Диапазон х > 0 и y < 0");
+    else Console.WriteLine ("неправильный ввод");
+    }
+
+Console.WriteLine ("Введите номер четверти:");
+int num = Convert.ToInt32 (Console.ReadLine ());
+ShowDiapazone (num);
+
